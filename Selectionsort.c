f6712bcb354dbc151d3025e7adb081ac2102f1e6
KISHORE V <89207602+Kishore-189@users.noreply.github.com>
2023-02-02 21:58:48 +0530

    Create Selectionsort.c

---
+++ Selectionsort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[]={8,5,6,3,7,4,6};
+    for(int i=0;i<7;i++){
+        for(int j=i+1;j<7;j++){
+        int temp=0;
+        if(a[i]>a[j]){
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+        else{
+            continue;
+        }
+    }
+    }
+    
+    for(int i=0;i<7;i++)
+        printf("%d",a[i]);
+}
